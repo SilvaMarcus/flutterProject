e824838e9d9225e929a3b16a57a7b4600bd51552
Marcus-Silva <92951909+SilvaMarcus@users.noreply.github.com>
2023-03-27 17:31:59 -0300

    .

---
+++ flutter_perguntas/lib/main.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+main() => runApp(PerguntaApp());
+
+class PerguntaApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text(
+            'Perguntas',
+            textAlign: TextAlign.center,
+          ),
+          centerTitle: true,
+          backgroundColor: Colors.purpleAccent,
+        ),
+         body: Center(
+          child: ElevatedButton(
+            onPressed: () {
+              print('Olá mundo!');
+            },
+            child: Text('Clique aqui'),
+          ),
+        ),
+      ),
+    );
+  }
+}
+
